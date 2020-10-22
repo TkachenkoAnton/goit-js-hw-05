86313a590df979f673b6c04609c281c579666a4b
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-10-22 08:26:18 +0300

    добавляет решение 2 задачи

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,20 @@
-
+class User {
+    constructor(name, age, followers) {
+        this.name = name;
+        this.age = age;
+        this.followers = followers;
+    }
+    getInfo() {
+        return `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+    }
+}
   
 
 // console.log(typeof User);
 // 'function'
 
-// const mango = new User('Mango', 2, 20);
+const mango = new User('Mango', 2, 20);
+// console.log(mango)
 // console.log(mango.getInfo()); 
 // 'User Mango is 2 years old and has 20 followers'
 
